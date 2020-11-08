1bf59023f06b1f6a751a0ced8122d56e7225d25e
mariaalvarez11 <70607300+mariaalvarez11@users.noreply.github.com>
2020-11-08 15:50:16 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,43 @@
+var database;
+var form;
+var player;
+var game;
+var playerCount = 0
+var gameState = 0
+function setup(){
+  database = firebase.database();
+  console.log(database);
+  createCanvas(500,500);
+
+  game = new Game();
+  game.getState();
+  game.start();
+
+  /*hypnoticBall = createSprite(250,250,10,10);
+  hypnoticBall.shapeColor = "red";
+  var hypnoticBallPosition = database.ref('ball/position');
+  hypnoticBallPosition.on("value", readPosition, showError);*/
+}
+
+function draw(){
+  background("white");
+  
+}
+
+/*function writePosition(x,y){
+  database.ref('ball/position').set({
+    'x': position.x + x ,
+    'y': position.y + y
+  })
+}
+
+function readPosition(data){
+  position = data.val();
+  console.log(position.x);
+  hypnoticBall.x = position.x;
+  hypnoticBall.y = position.y;
+}
+
+function showError(){
+  console.log("Error in writing to the database");
+}*/
